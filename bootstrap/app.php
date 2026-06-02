dba32cd394ca7ba005b48651239ed13607f86574
Georgi Todorov <georgi.todorov@gameloft.com>
2026-06-02 11:00:47 +0300

    fix change locale

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,8 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             HandleAppearance::class,
-            HandleInertiaRequests::class,
             SetLocale::class,
+            HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
             SetTeamUrlDefaults::class,
         ]);
