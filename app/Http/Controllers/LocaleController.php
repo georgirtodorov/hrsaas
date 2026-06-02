80bf5eb0c8f43b75e732b61914fa2262ce6327d5
Georgi Todorov <georgi.todorov@gameloft.com>
2026-06-02 09:15:14 +0300

    validaiton of selected lang

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class LocaleController extends Controller
 {
     public function change(Request $request)
     {
-        $locale = $request->input('locale', config('app.fallback_locale'));
+        $validated = $request->validate([
+            'locale' => ['required', 'string', Rule::in(config('app.supported_locales', ['en', 'bg']))],
+        ]);
 
-        $request->session()->put('locale', $locale);
+        $request->session()->put('locale', $validated['locale']);
 
         return back();
     }
